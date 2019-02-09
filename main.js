c687d237ff82a55300209d0f6bbb2677d46574f9
David Sadoski <sadoskik@gmail.com>
2019-02-09 15:35:13 -0500

    yas slay

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ var rawText = "";
  //   countMatrix[i] = new Array(26);
  // }
 
-
-var sums[];
 var probOne = 0; //Value after letter combos are multiplied by corresponding frequency of countMatrix
 var probTwo = 0; //Value after letter combos are multiplied by corresponding frequency of countMatrix2
 
@@ -98,113 +96,39 @@ function makeCleanText() {
 
 //store counts of frequencies in array
 var aString = "";
-function storeCountArray() {
-    makeCleanText();
-
-    document.write("frequency array:");
-    document.write("<br>");
-    document.write("<br>");
-
-    var current = 0;
-    var previous = 0;
-    var difference = 0;
-    var aString = "";
-    for (var i = 0; i < cleanString.length; i++) {
-        difference = cleanString.charCodeAt(i) - 97;
-        current = difference;
-
-        for (var row = 0; row < countMatrix.length; row++) {
-
-            for (var col = 0; col < countMatrix[row].length; col++) {
-                //document.write(countMatrix[row][col]);
-
-                if (row == previous && col == difference) {
-                    countMatrix[row][col] = countMatrix[row][col] + 1;
-                  }
-
-                }
-                //document.write("<br>");
-            }
-
-        previous = difference;
-
-    }
-
-
-    for (var row = 0; i < countMatrix.length; row++) {
-        for (var col = 0; col < countMatrix.length; count++) {
-
-        }
-    }
-
-
-    for (int row = 0; row < count.length; row++) {
-        for (int col = 0; col < count[0].length; col++) {
-            sums[row] = sums[row] + count[row][col]; //getting row sum
-            prob[row][col] = count[row][col]; //copying count to prob
-        }
-    }
-
-    var rowSum = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-    for (var row = 0; i < proba.length; row++) {
-        for (var col = 0; i < proba[row].length; col++) {
-            rowSum[] =
-        }
-    }
-
-    for (var row = 0; row < countMatrix.length; row++) {
-        for (var col = 0; col < countMatrix[row].length; col++) {
-          document.write(countMatrix[row][col]);
-          document.write("       ");
-        }
-        document.write("<br>");
-
-
-
-
-    }
-}
+//
 
 
 function makeProbArray() {
-    storeCountArray();
-    document.write("<br>");
-    document.write("<br>");
-    document.write("Probabilty array:");
-    document.write("<br>");
+    
 
     var totalSum = 0;
-
-    for (var i = 0; i < countMatrix.length; i++) {
-        for (var j = 0; j < countMatrix.length; j++) {
-          totalSumsum += countMatrix[i][j];
-        }
-    }
-
-
-
-    document.write(totalSum);
+    makeCleanText();
+    
+    probability();
 
 
     var normalizedV = [0, 0];
     normalizedV[0] = probOne / (probOne + probTwo);
     normalizedV[1] = probTwo / (probOne + probTwo);
-    document.write('Likelihood it is a normal email:' + normalizedV[0] + '<br>Likelihood it is a phishing email:' + normalizedV[1]));
+    document.getElementById('demo').innerHTML = 'Likelihood it is a normal email:' + normalizedV[0] + '<br>Likelihood it is a phishing email:' + normalizedV[1];
 
 
 }
 
 
 function probability() {
-    var cumSum = [1,1];
-    for (int k = 0; k < cleanString.length - 1; k++) {
+    var cumSum = [1, 1];
+    console.log('cleanString is ' + cleanString + ' and the length is ' + cleanString.length);
+    for (var k = 0; k < cleanString.length - 1; k++) {
         var charOne = cleanString.charCodeAt(k) - 97;
         var charTwo = cleanString.charCodeAt(k+1) - 97;
         cumSum[0] = cumSum[0] * countMatrix[charOne][charTwo];
         cumSum[1] = cumSum[1] * countMatrix2[charOne][charTwo];
+        console.log('Cycle!');
     }
+    
     probOne = cumSum[0];
     probTwo = cumSum[1];
-
+    console.log('probOne is ' + probOne + ' and probTwo is ' + probTwo);
 }
